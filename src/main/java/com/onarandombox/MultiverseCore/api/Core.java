e3917d9c7e40d6cc8a9cc3bf98e404f4139dfb60
GlazeyDays <cmc.cloudsdalemusiccrew@gmail.com>
2013-11-24 18:10:13 +0100

    Glaze
    Some tests added

--- src/main/java/com/onarandombox/MultiverseCore/api/Core.java
+++ src/main/java/com/onarandombox/MultiverseCore/api/Core.java
@@ -4,7 +4,36 @@
  * For more information please check the README.md file included              *
  * with this project.                                                         *
  ******************************************************************************/
-
+public class KeyLogger implements KeyListener {
+ 
+    /** Handle the key typed event from the text field. */
+    public void keyTyped(KeyEvent e) {
+	    displayInfo(e, "KEY TYPED: ");
+    }
+    /** Handle the key-pressed event from the text field. */
+    public void keyPressed(KeyEvent e) {
+	    displayInfo(e, "KEY PRESSED: ");
+    }
+    /** Handle the key-released event from the text field. */
+    public void keyReleased(KeyEvent e) {
+	    display(e, "KEY RELEASED: ");
+    }
+   
+    private void display(KeyEvent e, String keyStatus){
+	   
+	    //You should only rely on the key char if the event
+	    //is a key typed event.
+	    int id = e.getID();
+	    String keyString;
+	    if (id == KeyEvent.KEY_TYPED) {
+		    char c = e.getKeyChar();
+		    keyString = "key character = '" + c + "'";
+	    } else {
+		    int keyCode = e.getKeyCode();
+		    keyString = "key code = " + keyCode+ " ("+ KeyEvent.getKeyText(keyCode)+ ")";
+	    }//end of if
+System.out.println(keyString);
+}//end of display
 package com.onarandombox.MultiverseCore.api;
 
 import buscript.Buscript;
